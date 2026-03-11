931c76fd88ca77273285e9ba0436013105d04dd2
Debian <debian@mail.jsdconsult.cloud>
2026-03-11 15:24:28 +0200

    v1.1 - Toutenbus

---
+++ providers/gtfs/configurations/toutenbus_20.mjs
@@ -0,0 +1,25 @@
+/** @type {import('../src/model/source.ts').SourceOptions[]} */
+const sources = [
+	{
+		id: "toutenbus_20",
+		staticResourceHref: "https://gtfs-rt.infra-hubup.fr/toutenbus/current/gtfs",
+		realtimeResourceHrefs: ["https://gtfs-rt.infra-hubup.fr/toutenbus/realtime"],
+		getNetworkRef: () => "TOUTENBUS",
+		getOperatorRef: () => "TOUTENBUS",
+		getVehicleRef: (vehicle) => vehicle?.id ?? vehicle?.label,
+		getDestination: (journey, vehicle) => vehicle?.label ?? journey?.calls.at(-1)?.stop.name,
+	},
+];
+
+/** @type {import('../src/configuration/configuration.ts').Configuration} */
+const configuration = {
+	computeDelayMs: 20_000,
+	redisOptions: {
+		url: process.env.REDIS_URL ?? "redis://127.0.0.1:6379",
+		username: process.env.REDIS_USERNAME,
+		password: process.env.REDIS_PASSWORD,
+	},
+	sources,
+};
+
+export default configuration;
